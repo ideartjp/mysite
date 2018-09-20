a91987b375fd3486f5dad857d6b1dff6fc275566
ideart <ideart.jp@gmail.com>
2018-09-21 00:50:58 +0900

    forで1 - 9までの配列を作成、foreachを使って表示

--- msql/multiplication_table.php
+++ msql/multiplication_table.php
@@ -9,26 +9,43 @@
   <div class="multiplication_table_wrapper">
     <div class="container">
       <div class="multiplication_table">
+
         <table>
-        <?php // 1両目・11列目に色をつける、2列目・2行目以降は奇数行・列に色をつける
-
+        <?php // forで1-9までの配列を作成、foreachを使って表示
         for($i = 1; $i < 10; $i++){
-          echo '<tr>';
+            // echo $i;
+            echo '<tr>';
             for($j = 1; $j < 10; $j++){
-                $x = $i * $j;
-                if ($i == 1 || $j == 1) {
-                  echo '<td style="background-color:rgba(7,191,163,0.3);">' . $x . '</td>';
-                } elseif ($i % 2 != 0 || $j % 2 != 0) {
-                  echo '<td style="background-color:rgba(7,191,163,0.3);">' . $x . '</td>';
-                } else {
-                  echo "<td>$x</td>";
-                }
+              $lines = $i * $j;
+              foreach ((array)$lines as $line) {
+                echo "<td>$line</td>";
+              }
             }
-          echo '</tr>';
+            echo '</tr>';
         }
-
         ?>
         </table>
+
+        <!-- <table>
+         <?php // 1行目・1列目に色をつける、2行目・2列目以降は奇数の行列に色をつける
+
+        // for($i = 1; $i < 10; $i++){
+        //   echo '<tr>';
+        //     for($j = 1; $j < 10; $j++){
+        //         $x = $i * $j;
+        //         if ($i == 1 || $j == 1) {
+        //           echo '<td style="background-color:rgba(7,191,163,0.3);">' . $x . '</td>';
+        //         } elseif ($i % 2 != 0 || $j % 2 != 0) {
+        //           echo '<td style="background-color:rgba(7,191,163,0.3);">' . $x . '</td>';
+        //         } else {
+        //           echo "<td>$x</td>";
+        //         }
+        //     }
+        //   echo '</tr>';
+        // }
+        //
+        // ?>
+        </table> -->
 
         <!-- <table>
         <?php // 配列は使ってないけど、foreachも使わないバージョンができた(笑)

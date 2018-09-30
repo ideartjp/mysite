ec33ff3f006811c0b7999a8f3a9441de25eda017
ideart <ideart.jp@gmail.com>
2018-09-30 12:22:44 +0900

    validation, session処理を追加

--- confirm.php
+++ confirm.php
@@ -1,4 +1,110 @@
 <?php
+session_start();
+
+$validFlag = true;
+
+// ===== validation check =====
+// basic
+if (!isset($_POST['familyName']) || $_POST['familyName'] == '') {
+    $validFlag = false;
+}
+if (!isset($_POST['givenName']) || $_POST['givenName'] == '') {
+    $validFlag = false;
+}
+if (!isset($_POST['familyNameKana']) || $_POST['familyNameKana'] == '') {
+    $validFlag = false;
+}
+if (!isset($_POST['givenNameKana']) || $_POST['givenNameKana'] == '') {
+    $validFlag = false;
+}
+if (!isset($_POST['zipcode']) || $_POST['zipcode'] == '') {
+    $validFlag = false;
+}
+if (!isset($_POST['prefecture']) || $_POST['prefecture'] == '') {
+    $validFlag = false;
+}
+if (!isset($_POST['address']) || $_POST['address'] == '') {
+    $validFlag = false;
+}
+if (!isset($_POST['phone']) || $_POST['phone'] == '') {
+    $validFlag = false;
+}
+if (!isset($_POST['email']) || $_POST['email'] == '') {
+    $validFlag = false;
+}
+// representative
+if (!isset($_POST['representativeStay']) || $_POST['representativeStay'] == '') {
+  if (!isset($_POST['representativeFamilyName']) || $_POST['representativeFamilyName'] == '') {
+      $validFlag = false;
+  }
+  if (!isset($_POST['representativeGivenName']) || $_POST['representativeGivenName'] == '') {
+      $validFlag = false;
+  }
+  if (!isset($_POST['representativeFamilyNameKana']) || $_POST['representativeFamilyNameKana'] == '') {
+      $validFlag = false;
+  }
+  if (!isset($_POST['representativeGivenNameKana']) || $_POST['representativeGivenNameKana'] == '') {
+      $validFlag = false;
+  }
+  if (!isset($_POST['representativeZipcode']) || $_POST['representativeZipcode'] == '') {
+      $validFlag = false;
+  }
+  if (!isset($_POST['representativePrefecture']) || $_POST['representativePrefecture'] == '') {
+      $validFlag = false;
+  }
+  if (!isset($_POST['representativeAddress']) || $_POST['representativeAddress'] == '') {
+      $validFlag = false;
+  }
+  if (!isset($_POST['representativePhone']) || $_POST['representativePhone'] == '') {
+      $validFlag = false;
+  }
+}
+// others
+if (!isset($_POST['checkInTime']) || $_POST['checkInTime'] == '') {
+    $validFlag = false;
+}
+if (!isset($_POST['transportation']) || $_POST['transportation'] == '') {
+    $validFlag = false;
+}
+if (!isset($_POST['guestNumMr']) || $_POST['guestNumMr'] == '') {
+    $validFlag = false;
+}
+if (!isset($_POST['guestNumMrs']) || $_POST['guestNumMrs'] == '') {
+    $validFlag = false;
+}
+// $validFlag == false
+if ($validFlag == false) {
+  $_SESSION['familyName']     = $_POST['familyName'];
+  $_SESSION['givenName']      = $_POST['givenName'];
+  $_SESSION['familyNameKana'] = $_POST['familyNameKana'];
+  $_SESSION['givenNameKana']  = $_POST['givenNameKana'];
+  $_SESSION['zipcode']        = $_POST['zipcode'];
+  $_SESSION['prefecture']     = $_POST['prefecture'];
+  $_SESSION['address']        = $_POST['address'];
+  $_SESSION['phone']          = $_POST['phone'];
+  $_SESSION['email']          = $_POST['email'];
+  // representative
+  $_SESSION['representativeStay']           = $representativeStay;
+  $_SESSION['representativeFamilyName']     = $_POST['representativeFamilyName'];
+  $_SESSION['representativeGivenName']      = $_POST['representativeGivenName'];
+  $_SESSION['representativeFamilyNameKana'] = $_POST['representativeFamilyNameKana'];
+  $_SESSION['representativeGivenNameKana']  = $_POST['representativeGivenNameKana'];
+  $_SESSION['representativeZipcode']        = $_POST['representativeZipcode'];
+  $_SESSION['representativePrefecture']     = $_POST['representativePrefecture'];
+  $_SESSION['representativeAddress']        = $_POST['representativeAddress'];
+  $_SESSION['representativePhone']          = $_POST['representativePhone'];
+  // $_SESSION['representativeEmail']         = $_POST['representativeEmail'];
+  // others
+  $_SESSION['checkInTime']    = $_POST['checkInTime'];
+  $_SESSION['transportation'] = $_POST['transportation'];
+  $_SESSION['guestNumMr']     = $_POST['guestNumMr'];
+  $_SESSION['guestNumMrs']    = $_POST['guestNumMrs'];
+  // $_SESSION['contact']        = $_POST['contact'];
+  header("location: reserve.php");
+  exit();
+}
+
+// ===== $_POST =====
 // basic
 $familyName     = $_POST['familyName'];
 $givenName      = $_POST['givenName'];
@@ -19,12 +125,13 @@ $representativeZipcode        = $_POST['representativeZipcode'];
 $representativePrefecture     = $_POST['representativePrefecture'];
 $representativeAddress        = $_POST['representativeAddress'];
 $representativePhone          = $_POST['representativePhone'];
-//  - end -
-$checkIn        = $_POST['checkIn'];$checkIn = $_POST['checkIn'];
+// $representativeEmail          = $_POST['representativeEmail'];
+// others
+$checkInTime    = $_POST['checkInTime'];
 $transportation = $_POST['transportation'];
 $guestNumMr     = $_POST['guestNumMr'];
 $guestNumMrs    = $_POST['guestNumMrs'];
-
+$contact        = $_POST['contact'];
 // payment
 $payment      = $_POST['payment'];
 $paymentName  = $_POST['paymentName'];
@@ -32,8 +139,35 @@ $paymentNum   = $_POST['paymentNum'];
 $paymentMonth = $_POST['paymentMonth'];
 $paymentYear  = $_POST['paymentYear'];
 $paymentSecur = $_POST['paymentSecur'];
-$contact = $_POST['contact'];
 
+// ===== $_SESSION =====
+// basic
+$_SESSION['familyName']     = $_POST['familyName'];
+$_SESSION['givenName']      = $_POST['givenName'];
+$_SESSION['familyNameKana'] = $_POST['familyNameKana'];
+$_SESSION['givenNameKana']  = $_POST['givenNameKana'];
+$_SESSION['zipcode']        = $_POST['zipcode'];
+$_SESSION['prefecture']     = $_POST['prefecture'];
+$_SESSION['address']        = $_POST['address'];
+$_SESSION['phone']          = $_POST['phone'];
+$_SESSION['email']          = $_POST['email'];
+// representative
+$_SESSION['representativeStay']           = $representativeStay;
+$_SESSION['representativeFamilyName']     = $_POST['representativeFamilyName'];
+$_SESSION['representativeGivenName']      = $_POST['representativeGivenName'];
+$_SESSION['representativeFamilyNameKana'] = $_POST['representativeFamilyNameKana'];
+$_SESSION['representativeGivenNameKana']  = $_POST['representativeGivenNameKana'];
+$_SESSION['representativeZipcode']        = $_POST['representativeZipcode'];
+$_SESSION['representativePrefecture']     = $_POST['representativePrefecture'];
+$_SESSION['representativeAddress']        = $_POST['representativeAddress'];
+$_SESSION['representativePhone']          = $_POST['representativePhone'];
+// $_SESSION['representativeEmail']         = $_POST['representativeEmail'];
+// others
+$_SESSION['checkInTime']    = $_POST['checkInTime'];
+$_SESSION['transportation'] = $_POST['transportation'];
+$_SESSION['guestNumMr']     = $_POST['guestNumMr'];
+$_SESSION['guestNumMrs']    = $_POST['guestNumMrs'];
+$_SESSION['contact']        = $_POST['contact'];
 ?>
 
 <!DOCTYPE html>
@@ -43,7 +177,8 @@ $contact = $_POST['contact'];
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <title>ご予約内容の確認｜小口旅館</title>
-  <link rel="stylesheet" type="text/css" href="http://yui.yahooapis.com/3.18.1/build/cssreset-context/cssreset-context-min.css">
+  <link href="css/reset.css" rel="stylesheet">
+  <link href="css/common.css" rel="stylesheet">
   <link href="css/confirm.css" rel="stylesheet">
 </head>
 <body>
@@ -55,10 +190,10 @@ $contact = $_POST['contact'];
   <div class="confirm-wrapper">
     <div class="container">
       <div class="form-title">
-        <h2>ご予約内容の最終確認と確定をお願いいたします。</h2>
+        <h1>ご予約内容の最終確認と確定をお願いいたします。</h1>
       </div>
       <div class="confirm-form">
-        <form action="complete.php" method="post">
+        <form action="register.php" method="post">
           <table>
             <tr class="form-item">
               <th>氏名</th>
@@ -109,10 +244,11 @@ $contact = $_POST['contact'];
               <th>電話番号</th>
               <td><?= $representativePhone; ?></td>
             </tr>
+            <!-- <input type="hidden" name="representativeEmail" value="none"> -->
             <!-- $representative -->
             <tr class="form-item">
               <th>チェックイン予定時刻</th>
-              <td><?= $checkIn; ?></td>
+              <td><?= $checkInTime; ?></td>
             </tr>
             <tr class="form-item">
               <th>交通手段</th>

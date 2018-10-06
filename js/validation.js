6bd47e504784312f47c88f1d667ce6d7e8137efc
ideart <ideart.jp@gmail.com>
2018-10-06 11:46:44 +0900

    validation.jsを新規作成

---
+++ js/validation.js
@@ -0,0 +1,65 @@
+// フォームの入力チェック
+// familyName
+// $("input[name='familyName']").blur(function(){
+//   if(!$(this).val().match(/^[ぁ-んァ-ヶー一-龠 　\r\n\t]+$/) || $(this).val() == "" || $(this).val().match(/^[ 　\r\n\t]*$/)){
+//     alert('不正な値です。');
+//   }
+// });
+// givenName
+// $("input[name='givenName']").blur(function(){
+//   if(!$(this).val().match(/^[ぁ-んァ-ヶー一-龠 　\r\n\t]+$/) || $(this).val() == "" || $(this).val().match(/^[ 　\r\n\t]*$/)){
+//     alert('不正な値です。');
+//   }
+// });
+// familyNameKana
+// $("input[name='familyNameKana']").blur(function(){
+//   if(!$(this).val().match(/^[ぁ-ん 　\r\n\t]+$/) || $(this).val() == "" || $(this).val().match(/^[ 　\r\n\t]*$/)){
+//     alert('');
+//   }
+// });
+// givenNameKana
+// $("input[name='givenName']").blur(function(){
+//   if(!$(this).val().match(/^[ぁ-ん 　\r\n\t]+$/) || $(this).val() == "" || $(this).val().match(/^[ 　\r\n\t]*$/)){
+//     alert('');
+//   }
+// });
+// zipcode
+// $("input[name='zipcode']").blur(function(){
+//   if(!$(this).val().match(/^[0-9\-]+$/) || $(this).val() == "" || $(this).val().match(/^[ 　\r\n\t]*$/)){
+//     alert('半角数字とハイフンのみでご記入ください。');
+//   }
+// });
+// prefecture
+// $("input[name='prefecture']").blur(function(){
+//   if(!$(this).val().match(/^[ぁ-んァ-ヶー一-龠 　\r\n\t]+$/) || $(this).val() == "" || $(this).val().match(/^[ 　\r\n\t]*$/)){
+//     alert('');
+//   }
+// });
+// address
+// $("input[name='address']").blur(function(){
+//   if(!$(this).val().match(/^[ぁ-んァ-ヶー一-龠 　\r\n\t]+$/) || $(this).val() == "" || $(this).val().match(/^[ 　\r\n\t]*$/)){
+//     alert('');
+//   }
+// });
+// phone
+// $("input[name='phone']").blur(function(){
+//   if(!$(this).val().match(/^[0-9\-]+$/) || $(this).val() == "" || $(this).val().match(/^[ 　\r\n\t]*$/)){
+//     alert('半角数字とハイフンのみでご記入ください。');
+//   }
+// });
+// email
+// $("input[name='email']").blur(function(){
+//   if(!$(this).val().match(/^([a-zA-Z0-9])+([a-zA-Z0-9\._-])*@([a-zA-Z0-9_-])+([a-zA-Z0-9\._-]+)+$/) || $(this).val() == "" || $(this).val().match(/^[ 　\r\n\t]*$/)){
+//     alert('メールアドレスの形式が不正です。');
+//   }
+// });
+// guestNumMr $guestNumMrs
+
+// contact
+// $("textarea[name='hoge']").blur(function(){
+//   if($(this).val().match(/[<(.*)>.*<\/\1>]/)){
+//     alert('HTMLコードが含まれます。')
+//   } if else (!$(this).val().match(/^[ 　\r\n\t]*$/)){
+//     alert('改行、タブ、スペースだけの入力値は不可です。')
+// }
+// });
